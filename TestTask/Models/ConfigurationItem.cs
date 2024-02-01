8f70b37b37ff7b005164644ca1495ef19ec80cbd
HlibPavlyk <pavlikgo78@gmail.com>
2024-02-01 15:55:59 +0200

    Add project files.

---
+++ TestTask/Models/ConfigurationItem.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestTask.Models
+{
+    public class ConfigurationItem
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Value { get; set; }
+        public int? ParentId { get; set; }
+
+        public ConfigurationItem Parent { get; set; }
+        public List<ConfigurationItem> Children { get; set; }
+    }
+
+
+}
